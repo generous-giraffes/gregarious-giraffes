8782f11ce7b2757d415524e54b10ff9fb403bd2e
Laurhens D <drhens@gmail.com>
2016-11-02 18:51:25 -0400

    add public directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = {
   entry: ['./client/App.js'],
   output: {
-    path: path.resolve(__dirname, "client"),
+    path: path.resolve(__dirname, "client/public"),
     filename: 'bundle.js'
   },
   debug: true,
