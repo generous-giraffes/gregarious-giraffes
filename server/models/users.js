764385224c5df629ab58a2575f6222f7bdfa4035
JakeSmith1 <jakesmith5757@gmail.com>
2016-11-02 20:09:27 -0400

    feature(db): created models for mysql db to be used in the controller methods

--- server/models/users.js
+++ server/models/users.js
@@ -1,9 +1,14 @@
 var db = require('../db/index.js');
+//models interact with the database
+
+              // CHANGE THIS ALL TO KNEX!!! PLEASE!
 
 module.exports = {
   users: {
-    get: function (callback) {
-      // get all users: name, interests, , id
+    //the callback that will be passed in is a function that throws an error if there is an error
+    // or sends the results of the query as a json object
+    get: function(callback) {
+      //fill in ____ to make the query, included from, on and order by as an example
       var queryStr = 'select ______, \
                       from users ____ on (_____) \
                       order by ____ desc';
@@ -11,13 +16,18 @@ module.exports = {
         callback(err, results);
       });
     },
-    post: function (params, callback) {
-      // create a user
-      var queryStr = 'insert into users(_____) \
-                      value (_____)';
+    //look in db/index.js to see how the usertable is constructed
+    //id is ? because it is autoincremented
+    post: function(params, callback) {
+      var post  = {id: 1, title: 'Hello MySQL'};
+      var query = connection.query('INSERT INTO posts SET ?', post, function(err, result) {
+      });
+      //UPDATE THIS
+      var queryStr = 'insert into users(?, userid, userName, faveFood) \
+                      value ?,' + params;
       db.query(queryStr, params, function(err, results) {
         callback(err, results);
       });
     }
-  },
+  }
 }
