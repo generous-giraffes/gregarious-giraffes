ca2001ad03315aed304e42f49fa9949933250f16
Sara Sailors <sarasailors@gmail.com>
2016-11-07 16:56:49 -0500

    fix(cleanup): removed some extra info

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,16 +1,12 @@
 import { combineReducers } from 'redux';
 import UserReducer from './reducer_user';
 import ValidateUserFieldsReducer from './reducer_validateUserFields';
-import ResendEmailReducer from './reducer_resendEmail';
-import UpdateEmailReducer from './reducer_updateEmail';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
     user: UserReducer,
     validateFields: ValidateUserFieldsReducer,
-    form: formReducer, // <-- redux-form
-    resendEmail: ResendEmailReducer,
-    updateEmail: UpdateEmailReducer
+    form: formReducer // <-- redux-form
 });
 
 export default rootReducer;
