00c06d2a4b88a2ceed1cfda026c5ca58298acaa0
Sara Sailors <sarasailors@gmail.com>
2016-11-08 10:01:20 -0500

    chore(removing_email_conf): continuing to test auth, removing unnecessary code

--- client/views/auth/App.js
+++ client/views/auth/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Component } from 'react';
 import AppContainer from '../../container/AppContainer';
 
+//Import the main scss file
+import style from '../../styles/style.scss';
+
 class App extends Component {
     render() {
         return (
