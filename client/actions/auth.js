a0085e0de7a8ce48665384799af726858b78cce4
JakeSmith1 <jakesmith5757@gmail.com>
2016-11-10 16:15:42 -0500

    refactor(authReducer): renamed action and payload, changed spacing

--- client/actions/auth.js
+++ client/actions/auth.js
@@ -2,82 +2,52 @@ import axios from 'axios';
 
 export const SIGNUP_USER = 'SIGNUP_USER';
 export const LOGIN_USER = 'LOGIN_USER';
-
-const testDispatch = (data) => {
+//action to be dispatched if signInUser is successful
+const signInUserSuccess = (data) => {
   return {
     type: LOGIN_USER,
     data
   }
 };
-
+//function that sends user info to db and sends the user's data to the auth reducer
 export function signupUser(name, email, password) {
-    const request = axios.post('/api/signup', {
-        user: {
-            name: name,
-            email: email,
-            password: password
-        }
-    }).catch((response) => {
-        if (response instanceof Error) {
-            console.error('POST ERROR response', response);
-        } else {
-            console.log('POST ERROR server', response);
-        }
-    });
+  const request = axios.post('/api/signup', {
+    user: {
+      name: name,
+      email: email,
+      password: password
+    }
+  }).catch((response) => {
+      if (response instanceof Error) {
+          console.error('POST ERROR response', response);
+      } else {
+          console.log('POST ERROR server', response);
+      }
+  });
 
-    return {
-        type: SIGNUP_USER,
-        payload: request
-    };
+  return {
+      type: SIGNUP_USER,
+      payload: request
+  };
 }
-
+//function that sends user info to server to check if it matches info in the db. If it does an action is dispatched to the auth reducer to update the state to authenticate the user
 export function signinUser(email, password) {
   return (dispatch) => {
     return axios.post('/api/login', {
-            user: {
-                email: email,
-                password: password
-            }
-        })
-      .then(response => {
-        // Dispatch a synchronous action
-        // to handle data
-        let bigBadPayload = {
+      user: {
+        email: email,
+        password: password
+      }
+    }).then(response => {
+        let payload = {
           token: response.headers.token,
           name: response.data[0].name,
           email: response.data[0].email
         };
-        console.log('dipsathced in auth actions, going to reducer? bigBadPayload', bigBadPayload);
-        dispatch(testDispatch(bigBadPayload))
-      })
-      .catch(error => {
-        throw(error);
+        // Dispatch a synchronous action to handle payload
+        dispatch(signInUserSuccess(payload))
+      }).catch(error => {
+          throw(error);
       });
   };
 };
-    // const request = axios.post('/api/login', {
-    //         user: {
-    //             email: email,
-    //             password: password
-    //         }
-    //     })
-    //     //.then((response) => {
-    //     //    console.log(response, "this is JAKES FAVORITE ONE");
-    //     //})
-    //     .catch((response) => {
-    //         if (response instanceof Error) {
-    //             console.error('POST ERROR RESPONSE', response);
-    //         } else {
-    //             console.log('POST ERROR SERVER', response);
-    //         }
-    //     });
-    //
-    // //let data = request.data;
-    // //console.log(request, 'this is the request action/auth.js line 44');
-    // //console.log(data, 'this is the data action/auth.js line 44');
-    // console.log(request, 'this is the request.data');
-    // return {
-    //     type: LOGIN_USER,
-    //     payload: request
-    // };
-// }
