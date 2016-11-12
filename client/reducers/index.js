305e8fc4fda74db20b46661c3419a4eb9f55b551
JakeSmith1 <jakesmith5757@gmail.com>
2016-11-11 20:45:29 -0500

    git status

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import { routeReducer } from 'react-router-redux';
 import { auth_Reducer } from './reducer_auth';
 import { petApi_Reducer } from './reducer_petApi';
 
 
 const rootReducer = combineReducers({
-    routing: routeReducer,
     getPets: petApi_Reducer,
     isAuthorized: auth_Reducer
 });
