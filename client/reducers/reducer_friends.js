03cc945b2e29a7233af744d4c9fa7680a9c90ef0
JakeSmith1 <jakesmith5757@gmail.com>
2016-11-15 14:22:22 -0500

    refactor(friendReducer):unused code removed

--- client/reducers/reducer_friends.js
+++ client/reducers/reducer_friends.js
@@ -3,12 +3,8 @@ import { GET_FRIENDS } from '../actions/friends';
 export function friends_Reducer(state = { allFriends: [], newFriends: [] }, action) {
   switch (action.type) {
     case GET_FRIENDS:
-    console.log(action.payload, 'action friends payload+++++++++');
-      // let prevNofFriends = state.allFriends.length;
-      // let newFriends = state.allFriends.slice(prevNofFriends);
       let allFriends = action.payload;
       return Object.assign({}, state, {allFriends});
-
     default:
       return state;
   }
