84ecaa660456f77db2dcbc680ed57e95f4a0ebeb
Sara Sailors <sarasailors@gmail.com>
2016-11-17 10:57:21 -0500

    feat(photos): started adding photos and webpack for combining assets

--- client/components/App.js
+++ client/components/App.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
+
+
 //Import the main scss file
 import style from '../styles/style.scss';
+import img from '../styles/assets/pawprint.png';
+import img2 from '../styles/assets/sadpup.jpg';
 
 export default class App extends Component {
     render() {
