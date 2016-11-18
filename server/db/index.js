4a8f8195e749d0513aae410f7af9fd7c1c820d8c
Sara Sailors <sarasailors@gmail.com>
2016-11-18 15:38:25 -0500

    fix(index.js): ongoing fix with a vs admin

--- server/db/index.js
+++ server/db/index.js
@@ -9,7 +9,7 @@ var knex = require('knex')({
   connection: {
     host: 'localhost',
     user: 'root',
-    password: 'admin',//your local password for root user
+    password: 'a',//your local password for root user
     database: 'giraffeLocal'
   }
 });
